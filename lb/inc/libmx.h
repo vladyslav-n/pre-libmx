e4deb7a34267550cb8faafceae2fe1a40bb57db2
Vladyslav Nazarenko <fonclhe@gmail.com>
2019-11-13 00:25:39 +0200

    in

--- lb/inc/libmx.h
+++ lb/inc/libmx.h
@@ -62,4 +62,16 @@ void skip_spaces(const char *str, int *i);
 void skip_untilspace(const char *str, int *i);
 void skipchar(const char *str, char c, int *i);
 bool is_substr(const char *haystack, const char *needle);
+t_list *mx_create_node(void *data);
+void mx_push_front(t_list **list, void *data);
+void mx_push_back(t_list **list, void *data);
+void mx_pop_front(t_list **head);
+void mx_pop_back(t_list **head);
+int mx_list_size(t_list *list);
+t_list *mx_sort_list(t_list *lst, bool (*cmp)(void *, void *));
+
+typedef struct s_list {
+void *data;
+struct s_list *next;
+} t_list;
 
