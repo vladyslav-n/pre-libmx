b9dd52e6a12c80aaf6440f3d27d30e882df18203
Vladyslav Nazarenko <fonclhe@gmail.com>
2019-11-13 19:43:55 +0200

    in

---
+++ lb/sortlist/mx_sort_list.c
@@ -0,0 +1,9 @@
+#include "../inc/libmx.h"
+// #ifdef DEBUG
+// #endif
+
+t_list *mx_sort_list(t_list *lst, bool (*cmp)(void *, void *))
+{
+    
+}
+
--- libmx/src/mx_quicksort.c
+++ libmx/src/mx_quicksort.c
@@ -10,39 +10,36 @@ void swap(char **s1, char **s2)
     *s2 = tmp;
 }
 
-void quicksort_void(char **arr, int left, int right, int *count)
-{
-    if (right - left < 1)
-        return;
-    int pivot  = mx_strlen(arr[right]);
-    int wall = left;
-    for (int i = left; i < right; i++)
-    {
-        if (mx_strlen(arr[i]) < pivot)
-        {
-            if (i != wall)
-            {
-            swap(&arr[i], &arr[wall]);
-            (*count)++;
-            }
-            wall++;
-        }
-    }
-    if (wall != right)
-    {
-        swap(&arr[right], &arr[wall]);
-        (*count)++;
-    }
-    quicksort_void(arr, left, wall - 1, count);
-    quicksort_void(arr, wall + 1, right, count);
-}
-
 int mx_quicksort(char **arr, int left, int right)
 {   
-    int count = 0;
     if (!arr)
         return -1;
-    quicksort_void(arr, left, right, &count);
+    int count = 0;
+    int i = left, j = right;
+    int pivot = mx_strlen(arr[(left + right) / 2]);
+
+    /* partition */
+
+    while (i <= j) {
+        while (mx_strlen(arr[i]) < pivot)
+            i++;
+        while (mx_strlen(arr[j]) > pivot)
+            j--;
+        if (i <= j) {
+            if (i != j) {
+                swap(&arr[i], &arr[j]);
+                count += 1;
+            }
+            i++;
+            j--;
+        }
+    }
+
+    /* recursion */
+    if (left < j)
+        count += mx_quicksort(arr, left, j);
+    if (i < right)
+        count += mx_quicksort(arr, i, right);
     return count;
 }
 
